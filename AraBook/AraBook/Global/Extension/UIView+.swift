5fec6a11978d96d09e2fb3381fee990f737cb349
ahra_ <ahravelop@gmail.com>
2024-09-24 16:18:36 +0900

    [Setting] #1 - extension 및 literals 추가

---
+++ AraBook/AraBook/Global/Extension/UIView+.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+.swift
+//  AraBook
+//
+//  Created by 고아라 on 9/24/24.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addSubviews(_ views: UIView...) {
+        views.forEach {
+            self.addSubview($0)
+        }
+    }
+}
