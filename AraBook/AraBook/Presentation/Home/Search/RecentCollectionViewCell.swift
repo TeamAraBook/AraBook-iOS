ba05fc765c4e3dc65d6da4432d6c03074af57ee9
ahra_ <ahravelop@gmail.com>
2024-10-09 01:45:27 +0900

    #10 feat: 최근검색어 뷰 구현

---
+++ AraBook/AraBook/Presentation/Home/Search/RecentCollectionViewCell.swift
@@ -0,0 +1,71 @@
+//
+//  RecentCollectionViewCell.swift
+//  AraBook
+//
+//  Created by 고아라 on 10/8/24.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class RecentCollectionViewCell: UICollectionViewCell {
+    
+    private let recentTitle = UILabel()
+    private let recentDelButton = UIButton()
+    private let recentStackView = UIStackView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUI()
+        setHierarchy()
+        setLayout()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+private extension RecentCollectionViewCell {
+     
+    func setUI() {
+        self.backgroundColor = .white
+        self.makeCornerRound(radius: 12)
+        self.layer.borderColor = UIColor.gray300.cgColor
+        self.layer.borderWidth = 1
+        
+        recentTitle.do {
+            $0.textColor = .gray800
+            $0.textAlignment = .left
+            $0.font = .araFont(type: .PretandardRegular, size: 12)
+        }
+        
+        recentDelButton.do {
+            $0.setImage(.icClose, for: .normal)
+        }
+        
+        recentStackView.do {
+            $0.axis = .horizontal
+            $0.spacing = 4
+        }
+    }
+    
+    func setHierarchy() {
+        addSubview(recentStackView)
+        recentStackView.addArrangedSubviews(recentTitle,
+                                            recentDelButton)
+    }
+    
+    func setLayout() {
+        recentDelButton.snp.makeConstraints {
+            $0.size.equalTo(10)
+        }
+        
+        recentStackView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+        }
+    }
+}
