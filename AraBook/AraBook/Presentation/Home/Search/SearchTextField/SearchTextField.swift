50129baa41fb07a41bb4b056b53b9e834711ac35
ahra_ <ahravelop@gmail.com>
2024-10-09 01:46:01 +0900

    #10 fix: textField 입력 시 ui 수정

--- AraBook/AraBook/Presentation/Home/Search/SearchTextField/SearchTextField.swift
+++ AraBook/AraBook/Presentation/Home/Search/SearchTextField/SearchTextField.swift
@@ -5,7 +5,6 @@
 //  Created by 고아라 on 10/9/24.
 //
 
-
 import UIKit
 
 class SearchTextField: BaseTextField {
@@ -19,7 +18,7 @@ class SearchTextField: BaseTextField {
     override func configure() {
         super.configure()
         
-//        delegate = self
+        delegate = self
         borderStyle = .none
         textColor = .label
         font = .araFont(type: .PretandardRegular, size: 14)
@@ -35,8 +34,6 @@ class SearchTextField: BaseTextField {
         leftView = searchButton
         leftViewMode = .always
     }
-
-    // MARK: - Rect padding
     
     override func leftViewRect(forBounds bounds: CGRect) -> CGRect {
         var padding = super.leftViewRect(forBounds: bounds)
@@ -44,14 +41,10 @@ class SearchTextField: BaseTextField {
         return padding
     }
     
-    // MARK: - 입력 텍스트 inset 설정
-    
-    // editing 모드가 아닌 경우의 inset 값
     override func textRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.inset(by: UIEdgeInsets(top: 14.0, left: 38.0, bottom: 14.0, right: 0.0))
     }
     
-    // editing 모드인 경우의 inset 값
     override func editingRect(forBounds bounds: CGRect) -> CGRect {
         return bounds.inset(by: UIEdgeInsets(top: 14.0, left: 38.0, bottom: 14.0, right: 36.0))
     }
@@ -62,35 +55,17 @@ class SearchTextField: BaseTextField {
     
 }
 
-//extension SearchTextField: UITextFieldDelegate {
-//    
-//    func textFieldDidBeginEditing(_ textField: UITextField) {
-//        /// Focus mode
-//        
-//        /// 입력된 값이 없는 경우만 leftView(검색 이미지), rightView(클리어 버튼)를 보이지 않도록 설정
-//        if text?.isEmpty ?? true {
-//            rightViewMode = .never
-//        }
-//    }
-//    
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//        /// Resign mode
-//        
-//        // leftViewMode, rightViewMode를 .never로 설정하여 사라지게끔 하는 작업 가능
-//    }
-//    
-//    func textField(_ textField: UITextField,
-//                   shouldChangeCharactersIn range: NSRange,
-//                   replacementString string: String) -> Bool {
-//        
-//        /// newText: 새로 입력된 텍스트
-//        let newText = string.trimmingCharacters(in: .whitespacesAndNewlines)
-//        
-//        /// text: 기존에 입력되었던 text
-//        /// predictRange: 입력으로 예상되는 text의 range값 추측 > range값을 알면 기존 문자열에 새로운 문자를 위치에 알맞게 추가 가능
-////        guard let text = textField.text, let predictRange = Range(range, in: text) else { return true }
-////        
-////        print(text)
-//        return true
-//    }
-//}
+extension SearchTextField: UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField,
+                   shouldChangeCharactersIn range: NSRange,
+                   replacementString string: String) -> Bool {
+        
+        let currentText = textField.text ?? ""
+        let newText = (currentText as NSString).replacingCharacters(in: range, with: string)
+        
+        layer.borderColor = newText.isEmpty ? UIColor.gray800.cgColor : UIColor.mainGreen.cgColor
+        searchButton.setImage(newText.isEmpty ? .icSearch : .icSearchGreen, for: .normal)
+        return true
+    }
+}
