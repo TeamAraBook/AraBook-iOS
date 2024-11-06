10f89c45ba0572573acee154e1c84cb85c2a64ca
kwonjeong <kwonjeong9470@gmail.com>
2024-11-06 15:38:38 +0900

    [Fix] #41 - 마이페이지 배경화면 색 지정

--- AraBook/AraBook/Presentation/Mypage/MypageViewController.swift
+++ AraBook/AraBook/Presentation/Mypage/MypageViewController.swift
@@ -78,6 +78,7 @@ extension MypageViewController {
     
     func setUI() {
         self.navigationController?.navigationBar.isHidden = true
+        self.view.backgroundColor = .white
     }
     
     func setHierarchy() {
